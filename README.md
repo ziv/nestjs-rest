36c1f7da64c3f83a48eebd438db3e2dc837cfe6c
Ziv Perry <ziv.perry@gmail.com>
2025-07-29 12:14:05 +0300

    docs: update readme

--- README.md
+++ README.md
@@ -9,26 +9,52 @@ A [`JSON:API`](https://jsonapi.org/) compliant REST controller for
 
 ## Overview
 
+This project bring the `JSON:API` specification to the `NestJS` framework,
+providing almost _zero-config_ REST controller that supports CRUD operations,
+sorting, pagination, filtering, and more.
+
 ### Specification
 
-See https://jsonapi.org/ for the `JSON:API` specification.
+`JSON:API/v1` specification is a standard for building APIs in JSON format
+([reference](./packages/std-json-api/specs.md)).
+
+### Pagination Strategies
+
+There are two pagination strategies supported by this project:
+
+| Pagination Strategy     | Status | Description                                                                                          |
+| ----------------------- | ------ | ---------------------------------------------------------------------------------------------------- |
+| Offset-based pagination | ✈️     | Default pagination strategy using offset and limit query parameters.                                 |
+| Cursor-based pagination | 🚫     | Pagination strategy using cursor to provide a more efficient way to paginate through large datasets. |
 
 ### Implementation Report
 
 | Spec                | Subject    | Status | Reference                                       |
 | ------------------- | ---------- | ------ | ----------------------------------------------- |
 | content-negotiation |            | 🚫     | https://jsonapi.org/format/#content-negotiation |
-| document structure  |            | 🚧     | https://jsonapi.org/format/#document-structure  |
-| fetching data       |            | 🚧     | https://jsonapi.org/format/#fetching            |
+| document structure  |            | ✈️     | https://jsonapi.org/format/#document-structure  |
+| fetching data       |            | ✈️     | https://jsonapi.org/format/#fetching            |
 |                     | sorting    | ✈️     | https://jsonapi.org/format/#fetching-sorting    |
 |                     | pagination | ✈️     | https://jsonapi.org/format/#fetching-pagination |
 |                     | filtering  | ✈️     | https://jsonapi.org/format/#fetching-filtering  |
-| crud                |            | 🚧     | https://jsonapi.org/format/#crud                |
-| query parameters    |            | 🚧     | https://jsonapi.org/format/#query-parameters    |
-| errors              |            | 🚫     | https://jsonapi.org/format/#errors              |
+| crud                |            | ✈️     | https://jsonapi.org/format/#crud                |
+| query parameters    |            | ✈️     | https://jsonapi.org/format/#query-parameters    |
+| errors              |            | 🚧     | https://jsonapi.org/format/#errors              |
 | extensions          |            | 🫥     | https://jsonapi.org/extensions/                 |
 
-    ✈️ departured (ready for testing) 🚧 in progress 🚫 not started 🫥 not planned
+### Adapters Implementation Report
+
+| Adapter | Status | Description                                                    | Readme                                             |
+| ------- | ------ | -------------------------------------------------------------- | -------------------------------------------------- |
+| Mongodb | ✈️     | Simple (without relationships) adapter for Mongodb collection. | [readme](./packages/nestjs-rest-mongodb/readme.md) |
+
+---
+
+#### Legend
+
+    ✅ done ✈️ departed (ready for testing) 🚧 in progress 🚫 not started 🫥 not planned
+
+---
 
 # TODO
 
--- packages/demo/src/main.ts
+++ packages/demo/src/main.ts
@@ -1,72 +1,74 @@
-import {Controller, Get, Inject, Logger, Module} from "@nestjs/common";
-import {NestFactory} from "@nestjs/core";
+import { Controller, Get, Inject, Logger, Module } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 import MongodbAdapter from "nestjs-rest-mongodb";
-import {MongoClient} from "mongodb";
-import JsonApiController, {JsonApiControllerOptions,} from "nestjs-rest/controller";
+import { MongoClient } from "mongodb";
+import JsonApiController, {
+  JsonApiControllerOptions,
+} from "nestjs-rest/controller";
 
 @Controller("flags")
 export class Ctr extends JsonApiController {
-    constructor(@Inject("JSON_API_FLAGS") options: JsonApiControllerOptions) {
-        super(options);
-    }
+  constructor(@Inject("JSON_API_FLAGS") options: JsonApiControllerOptions) {
+    super(options);
+  }
 
-    @Get("favicon.ico")
-    favicon() {
-        // Handle favicon requests
-        return {message: "Favicon request received"};
-    }
+  @Get("favicon.ico")
+  favicon() {
+    // Handle favicon requests
+    return { message: "Favicon request received" };
+  }
 }
 
 @Module({
-    controllers: [Ctr],
-    providers: [
-        {
-            provide: "JSON_API_FLAGS",
-            useFactory: async () => {
-                const client = new MongoClient("mongodb://localhost:27017");
-                await client.connect();
-                const collection = client.db("flags").collection("flags");
-                const adapter = new MongodbAdapter({
-                    collection,
-                    id: "flags",
-                    // baseUrl: "http://localhost:3001",
-                    // projectItems: {
-                    //   key: 1,
-                    //   name: 1,
-                    // },
-                    // updateSchema: z.any(),
-                    // creationSchema: z.any(),
-                    // projectItem: {
-                    //   key: 1,
-                    //   name: 1,
-                    //   description: 1,
-                    //   createdAt: 1,
-                    //   updatedAt: 1,
-                    //   temporary: 1,
-                    //   archived: 1,
-                    //   appearances: 1,
-                    //   status: 1,
-                    //   statusLog: 1,
-                    //   statusType: 1,
-                    //   statusReason: 1,
-                    //   on: 1,
-                    // },
-                });
-                return {
-                    adapter,
-                    baseUrl: "http://localhost:3001",
-                    resourceId: "flags",
-                };
-            },
-        },
-    ],
+  controllers: [Ctr],
+  providers: [
+    {
+      provide: "JSON_API_FLAGS",
+      useFactory: async () => {
+        const client = new MongoClient("mongodb://localhost:27017");
+        await client.connect();
+        const collection = client.db("flags").collection("flags");
+        const adapter = new MongodbAdapter({
+          collection,
+          id: "flags",
+          // baseUrl: "http://localhost:3001",
+          // projectItems: {
+          //   key: 1,
+          //   name: 1,
+          // },
+          // updateSchema: z.any(),
+          // creationSchema: z.any(),
+          // projectItem: {
+          //   key: 1,
+          //   name: 1,
+          //   description: 1,
+          //   createdAt: 1,
+          //   updatedAt: 1,
+          //   temporary: 1,
+          //   archived: 1,
+          //   appearances: 1,
+          //   status: 1,
+          //   statusLog: 1,
+          //   statusType: 1,
+          //   statusReason: 1,
+          //   on: 1,
+          // },
+        });
+        return {
+          adapter,
+          baseUrl: "http://localhost:3001",
+          resourceId: "flags",
+        };
+      },
+    },
+  ],
 })
 export class App {
 }
 
 (async () => {
-    const app = await NestFactory.create(App);
+  const app = await NestFactory.create(App);
 
-    await app.listen(3001);
-    Logger.log("Server is running on http://localhost:3001", "Bootstrap");
+  await app.listen(3001);
+  Logger.log("Server is running on http://localhost:3001", "Bootstrap");
 })();
