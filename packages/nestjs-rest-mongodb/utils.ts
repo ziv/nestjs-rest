9d5a53caf6ddd465a4a222d0014e2be3023483b7
Ziv <ziv@users.noreply.github.com>
2025-07-29 17:54:05 +0300

    fix: remove unused file (#6)

--- packages/nestjs-rest-mongodb/utils.ts
+++
@@ -1,8 +0,0 @@
-import { attributes, identifier, resource } from "std-json-api/json-api";
-
-export function record2resource<T>(id: string, resourceId: string, attrs: T) {
-  return resource(
-    identifier(id, resourceId),
-    attributes(attrs),
-  );
-}
