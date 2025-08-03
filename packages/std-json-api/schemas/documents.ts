ff4bfab48fb0a49b97af4bb487b931c7966a90e5
Ziv Perry <ziv.perry@gmail.com>
2025-08-03 20:55:47 +0300

    fix(json-api): fix document schema

--- packages/std-json-api/schemas/documents.ts
+++ packages/std-json-api/schemas/documents.ts
@@ -1,42 +1,7 @@
 import {z} from "zod";
-import {AttributesObject, Links, Meta, PaginationLinks, ResourceObject} from "../../../tmp/json-api-types";
 import {metaObject} from "./meta-oject";
 import {links} from "./links";
 import {resourceData, resourceDataCollection, resourceNull} from "std-json-api/schemas/resource";
-
-
-export interface JsonApiObject {
-    version?: "1.0";
-    meta?: Meta;
-}
-
-export interface JsonApiDocumentBase {
-    jsonapi?: JsonApiObject;
-    links?: Links & PaginationLinks;
-    meta?: Meta;
-}
-
-/**
- * A JSON:API document containing a single resource or null.
- * @template T The type of the resource's attributes.
- */
-export interface SingleResourceDocument<
-    T extends AttributesObject = AttributesObject,
-> extends JsonApiDocumentBase {
-    data: ResourceObject<T> | null;
-    included?: ResourceObject[];
-}
-
-/**
- * A JSON:API document containing a collection of resources.
- * @template T The type of the resource's attributes.
- */
-export interface CollectionResourceDocument<
-    T extends AttributesObject = AttributesObject,
-> extends JsonApiDocumentBase {
-    data: ResourceObject<T>[];
-    included?: ResourceObject[];
-}
 
 export const jsonApiObject = z.object({
     version: z.string().optional(),
