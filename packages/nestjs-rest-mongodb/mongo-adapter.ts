710b9168822518713360c386b79b3b5864f77e6b
Ziv Perry <ziv.perry@zoominfo.com>
2026-02-02 20:51:32 +0200

    feat(nestjs-rest-mongodb): align api

--- packages/nestjs-rest-mongodb/mongo-adapter.ts
+++ packages/nestjs-rest-mongodb/mongo-adapter.ts
@@ -1,17 +1,9 @@
-import { debuglog } from "node:util";
-import type {
-  EstimatedDocumentCountOptions,
-  Filter,
-  MatchKeysAndValues,
-  OptionalUnlessRequiredId,
-} from "mongodb";
-import type {
-  JsonApiQuery,
-  JsonApiQueryFields,
-  JsonApiQueryFilter,
-} from "std-json-api";
-import type { JsonApiAdapter } from "nestjs-rest";
-import { Doc, InputId, MongodbAdapterOptions, normalize } from "./options";
+import {debuglog} from "node:util";
+import type {Filter, MatchKeysAndValues, OptionalUnlessRequiredId,} from "mongodb";
+import type {JsonApiQuery, JsonApiQueryFields, JsonApiQueryFilter,} from "std-json-api";
+import type {JsonApiAdapter} from "nestjs-rest";
+import type {Doc, InputId, MongodbAdapterOptions} from "./options";
+import {normalize} from "./options";
 
 const log = debuglog("nestjs-rest-mongodb");
 
@@ -22,93 +14,103 @@ const log = debuglog("nestjs-rest-mongodb");
  * Remember to index your collections based on the fields you query frequently for optimal performance.
  */
 export class MongoAdapter<T extends Doc> implements JsonApiAdapter<T> {
-  private readonly options: MongodbAdapterOptions<T>;
+    private readonly options: MongodbAdapterOptions<T>;
 
-  constructor(options: Partial<MongodbAdapterOptions<T>>) {
-    this.options = normalize(options);
-    log("MongoAdapter initialized with options: %O", this.options);
-  }
+    constructor(options: Partial<MongodbAdapterOptions<T>>) {
+        this.options = normalize(options);
+        log("MongoAdapter initialized with options: %O", this.options);
+    }
 
-  /**
-   * Count the total number of records matching the query.
-   * @param query
-   */
-  async count(query: JsonApiQuery): Promise<number> {
-    return await this.options.collection.estimatedDocumentCount(
-      query.filter as EstimatedDocumentCountOptions,
-    );
-  }
+    /**
+     * Count the total number of records matching the query.
+     * @param query
+     */
+    async count(query: JsonApiQuery): Promise<number> {
+        return await this.options.collection.countDocuments(
+            this.options.filterFactory(
+                query.filter as JsonApiQueryFilter,
+            ) as Filter<T>
+        );
+    }
 
-  /**
-   * Fetch multiple records based on the provided query.
-   * @param query
-   */
-  async multiple(query: JsonApiQuery): Promise<T[]> {
-    const filter = this.options.filterFactory(
-      query.filter as JsonApiQueryFilter,
-    ) as Filter<T>;
-    const sort = query.sort ?? { _id: 1 } as Record<string, 1 | -1>;
-    const limit = query.page && query.page.limit
-      ? query.page.limit
-      : this.options.defaultPageSize;
-    const offset = query.page && "offset" in query.page ? query.page.offset : 0;
+    /**
+     * Fetch multiple records based on the provided query.
+     * todo add cursor based pagination support
+     * @param query
+     */
+    async multiple(query: JsonApiQuery): Promise<T[]> {
+        const filter = this.options.filterFactory(
+            query.filter as JsonApiQueryFilter,
+        ) as Filter<T>;
+        const sort = query.sort ?? {_id: 1} as Record<string, 1 | -1>;
+        const limit = query.page && "limit" in query.page && query.page.limit > 0
+            ? query.page.limit
+            : this.options.defaultPageSize;
+        const offset = query.page && "offset" in query.page
+            ? query.page.offset
+            : 0;
+        const project = query.fields[this.options.descriptor.resourceId];
 
-    return await this.options
-      .collection
-      .find(filter)
-      .sort(sort)
-      .limit(limit)
-      .skip(offset)
-      .toArray() as T[];
-  }
+        let cursor = this.options.collection.find(filter);
 
-  /**
-   * Fetch a single record by its ID.
-   * @param id
-   * @param fields
-   */
-  async single(
-    id: string,
-    fields?: JsonApiQueryFields["item"],
-  ): Promise<T | null> {
-    return await this.options.collection.findOne(
-      this.options.uniqueFactory(id),
-      fields ? { projection: fields } : undefined,
-    ) as T | null;
-  }
+        if (project) {
+            cursor = cursor.project(project);
+        }
 
-  /**
-   * Create a new record with the provided data.
-   * @param data
-   */
-  async create<R = T>(data: R): Promise<string> {
-    const { insertedId } = await this.options.collection.insertOne(
-      data as OptionalUnlessRequiredId<T>,
-    );
-    return String(insertedId);
-  }
+        return await cursor
+            .sort(sort)
+            .limit(limit)
+            .skip(offset)
+            .toArray() as T[];
+    }
 
-  /**
-   * Update a record by its ID with the provided data.
-   * @param id
-   * @param data
-   */
-  async update<R = T>(id: InputId, data: Partial<R>): Promise<boolean> {
-    const res = await this.options.collection.updateOne(
-      this.options.uniqueFactory(id),
-      {
-        $set: data as MatchKeysAndValues<never>,
-      },
-    );
-    return res.modifiedCount === 1;
-  }
+    /**
+     * Fetch a single record by its ID.
+     * @param id
+     * @param fields
+     */
+    async single(
+        id: string,
+        fields?: JsonApiQueryFields["item"],
+    ): Promise<T | null> {
+        return await this.options.collection.findOne(
+            this.options.uniqueFactory(id),
+            fields ? {projection: fields} : undefined,
+        ) as T | null;
+    }
 
-  /**
-   * Delete a record by its ID.
-   * @param id
-   */
-  async remove(id: InputId): Promise<boolean> {
-    await this.options.collection.deleteOne(this.options.uniqueFactory(id));
-    return true;
-  }
+    /**
+     * Create a new record with the provided data.
+     * @param data
+     */
+    async create<R = T>(data: R): Promise<string> {
+        const {insertedId} = await this.options.collection.insertOne(
+            data as OptionalUnlessRequiredId<T>,
+        );
+        return String(insertedId);
+    }
+
+    /**
+     * Update a record by its ID with the provided data.
+     * @param id
+     * @param data
+     */
+    async update<R = T>(id: InputId, data: Partial<R>): Promise<boolean> {
+        const res = await this.options.collection.updateOne(
+            this.options.uniqueFactory(id),
+            {
+                $set: data as MatchKeysAndValues<never>,
+            },
+        );
+        return res.modifiedCount === 1;
+    }
+
+    /**
+     * Delete a record by its ID.
+     * @param id
+     */
+    async remove(id: InputId): Promise<boolean> {
+        await this.options.collection.deleteOne(this.options.uniqueFactory(id));
+        return true;
+    }
 }
--- packages/nestjs-rest-mongodb/options.ts
+++ packages/nestjs-rest-mongodb/options.ts
@@ -1,7 +1,7 @@
-import type { Collection, Document, MatchKeysAndValues } from "mongodb";
-import { ObjectId } from "mongodb";
-import type { Attributes, JsonApiQueryFilter } from "std-json-api";
-import type { ResourceDescriptor } from "nestjs-rest";
+import type {Collection, Document, MatchKeysAndValues} from "mongodb";
+import {ObjectId} from "mongodb";
+import type {Attributes, JsonApiQueryFilter} from "std-json-api";
+import type {ResourceDescriptor} from "nestjs-rest";
 
 export type Doc = Document & Attributes;
 export type InputId = string | string[];
@@ -10,102 +10,111 @@ export type InputId = string | string[];
  * Options for configuring the MongoDB adapter.
  */
 export type MongodbAdapterOptions<T extends Doc> = {
-  /**
-   * Resource descriptor defining the resource structure.
-   */
-  descriptor: ResourceDescriptor;
+    /**
+     * Resource descriptor defining the resource structure.
+     */
+    descriptor: ResourceDescriptor;
 
-  /**
-   * MongoDB collection for the resource.
-   */
-  collection: Collection<T>;
+    /**
+     * MongoDB collection for the resource.
+     */
+    collection: Collection<T>;
 
-  /**
-   * Related collections for handling relationships.
-   */
-  relatedCollections: { [key: string]: string };
+    /**
+     * Related collections for handling relationships.
+     */
+    relatedCollections: { [key: string]: string };
 
-  /**
-   * Factory function to create unique identifiers for documents.
-   * @param input
-   */
-  uniqueFactory: (input: InputId) => MatchKeysAndValues<T>;
+    /**
+     * Factory function to create unique identifiers for documents.
+     * @param input
+     */
+    uniqueFactory: (input: InputId) => MatchKeysAndValues<T>;
 
-  /**
-   * Optional factory function to create filter documents from JSON:API queries.
-   * @param query
-   */
-  filterFactory: (query?: JsonApiQueryFilter) => Document;
+    /**
+     * Optional factory function to create filter documents from JSON:API queries.
+     * @param query
+     */
+    filterFactory: (query?: JsonApiQueryFilter) => Document;
 
-  /**
-   * Default page size for pagination.
-   */
-  defaultPageSize: number;
+    /**
+     * Default page size for pagination.
+     */
+    defaultPageSize: number;
+
+    /**
+     * Maximum page size for pagination.
+     */
+    maxPageSize: number;
 };
 
 export function normalize<T extends Doc>(
-  options: Partial<MongodbAdapterOptions<T>>,
+    options: Partial<MongodbAdapterOptions<T>>,
 ) {
-  // assert required options
-  // ---------------------------
+    // assert required options
+    // ---------------------------
 
-  if (!options.descriptor) {
-    throw new Error("MongoAdapter requires ResourceDescriptor");
-  }
-
-  if (!options.collection) {
-    throw new Error("MongoAdapter requires a collection");
-  }
-
-  // normalize options
-  // ---------------------------
-
-  if (!options.uniqueFactory) {
-    const key = options.descriptor.idKey;
-
-    if (!key) {
-      // should not happen as idKey is required in ResourceDescriptor
-      throw new Error(
-        "MongoAdapter requires idKey in ResourceDescriptor to build uniqueFactory",
-      );
-    } else if (Array.isArray(key)) {
-      // composite key
-      options.uniqueFactory = (ids: InputId) => {
-        if (ids.length !== key.length) {
-          throw new Error(
-            `Composite key requires ${key.length} values, but got ${ids.length}`,
-          );
-        }
-        return key.reduce((acc, key) => {
-          // we can safely assert here as we checked the length before
-          acc[key] = (ids as string[]).shift()!;
-          return acc;
-        }, {} as Record<string, string>) as MatchKeysAndValues<T>;
-      };
-    } else if (key === "_id") {
-      // mongo object id
-      options.uniqueFactory = (
-        id: InputId,
-      ) => ({ _id: new ObjectId(id as string) } as MatchKeysAndValues<never>);
-    } else {
-      // simple key
-      options.uniqueFactory = (
-        id: InputId,
-      ) => ({ [key]: id } as MatchKeysAndValues<never>);
+    if (!options.descriptor) {
+        throw new Error("MongoAdapter requires ResourceDescriptor");
     }
-  }
 
-  if (!options.filterFactory) {
-    // if the user does not provide a filter factory
-    // the results can lead to injection attacks
-    // note: this is a very naive implementation
-    // note: this is sometime the required behavior for advanced usage, just be cautious
-    options.filterFactory = (filter?: JsonApiQueryFilter) => filter || {};
-  }
+    if (!options.collection) {
+        throw new Error("MongoAdapter requires a collection");
+    }
 
-  if (!options.defaultPageSize) {
-    options.defaultPageSize = 10;
-  }
+    // normalize options
+    // ---------------------------
 
-  return options as MongodbAdapterOptions<T>;
+    if (!options.uniqueFactory) {
+        const key = options.descriptor.idKey;
+
+        if (!key) {
+            // should not happen as idKey is required in ResourceDescriptor
+            throw new Error(
+                "MongoAdapter requires idKey in ResourceDescriptor to build uniqueFactory",
+            );
+        } else if (Array.isArray(key)) {
+            // composite key
+            options.uniqueFactory = (ids: InputId) => {
+                if (ids.length !== key.length) {
+                    throw new Error(
+                        `Composite key requires ${key.length} values, but got ${ids.length}`,
+                    );
+                }
+                return key.reduce((acc, key) => {
+                    // we can safely assert here as we checked the length before
+                    acc[key] = (ids as string[]).shift()!;
+                    return acc;
+                }, {} as Record<string, string>) as MatchKeysAndValues<T>;
+            };
+        } else if (key === "_id") {
+            // mongo object id
+            options.uniqueFactory = (
+                id: InputId,
+            ) => ({_id: new ObjectId(id as string)} as MatchKeysAndValues<never>);
+        } else {
+            // simple key
+            options.uniqueFactory = (
+                id: InputId,
+            ) => ({[key]: id} as MatchKeysAndValues<never>);
+        }
+    }
+
+    if (!options.filterFactory) {
+        // if the user does not provide a filter factory
+        // the results can lead to injection attacks
+        // note: this is a very naive implementation
+        // note: this is sometime the required behavior for advanced usage, just be cautious
+        options.filterFactory = (filter?: JsonApiQueryFilter) => filter || {};
+    }
+
+    if (!options.defaultPageSize) {
+        options.defaultPageSize = 10;
+    }
+
+    if (options.defaultPageSize > (options?.maxPageSize || 100)) {
+        options.defaultPageSize = options.maxPageSize || 100;
+    }
+
+    return options as MongodbAdapterOptions<T>;
 }
